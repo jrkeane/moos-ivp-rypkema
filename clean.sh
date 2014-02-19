68f3930138647f8e0baff246dd2315e6e1572394
nicrip <nick.rypkema@gmail.com>
2014-02-19 14:19:52 -0800

    added simple clean script

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#--- simple clean script ---#
+
+#remove all files in build directory
+rm -rf ./build/*
+#remove all files in lib directory
+rm -rf ./lib/*
+#remove all files in bin directory (except for original binaries)
+find ./bin/ -mindepth 1 ! -name "MOOSup.py" ! -name "MyGenMOOSApp" ! -name "README_MOOSup.py" -delete
+#remove all MOOSLog* folders
+find ./ -mindepth 1 -name "MOOSLog*" -type d -exec rm -rfv {} \;
